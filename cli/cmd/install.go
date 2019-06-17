dc1c9498a0345d00b2bc587188f303aa2dcce4a4
Armin Buerkle <armin.buerkle@alfatraining.de>
2019-06-17 13:49:41 +0200

    Set default cluster domain to 'cluster.local'
    Signed-off-by: Armin Buerkle <armin.buerkle@alfatraining.de>

--- cli/cmd/install.go
+++ cli/cmd/install.go
@@ -38,6 +38,7 @@ type (
 		stage string
 
 		Namespace                string
+		ClusterDomain            string
 		ControllerImage          string
 		WebImage                 string
 		PrometheusImage          string
@@ -576,6 +577,7 @@ func (options *installOptions) buildValuesWithoutIdentity(configs *pb.All) (*ins
 
 		// Controller configuration:
 		Namespace:              controlPlaneNamespace,
+		ClusterDomain:          defaultClusterDomain,
 		UUID:                   configs.GetInstall().GetUuid(),
 		ControllerReplicas:     options.controllerReplicas,
 		ControllerLogLevel:     options.controllerLogLevel,
@@ -768,6 +770,7 @@ func (options *installOptions) globalConfig(identity *pb.IdentityContext) *pb.Gl
 		Version:                options.controlPlaneVersion,
 		IdentityContext:        identity,
 		OmitWebhookSideEffects: options.omitWebhookSideEffects,
+		ClusterDomain:          defaultClusterDomain,
 	}
 }
 
